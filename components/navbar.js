2a445040a674f50653cd3f188fec0d85288a5303
kartik0603 <kartikhirapara800@gmail.com>
2024-06-17 13:58:38 +0530

    Royal Enfield

--- components/navbar.js
+++ components/navbar.js
@@ -1,6 +1,6 @@
 // components/navbar.js
 const navbar_RE = () => {
-  return `   <nav class="navbar navbar-expand-lg bg-black" id="navbar_RE">
+  return `   <nav class="navbar navbar-expand-lg bg-black position-fixed z-3 w-100" id="navbar_RE">
       <div class="container-fluid ">
           <a class="navbar-brand" href="/index.html">
               <img src="../Images/RE nav logo.png" alt="Logo" width="150" height="26" class="d-inline-block align-text-top ms-4">
